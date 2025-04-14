e12ebb4501b7aa0ad35cd5f095f4e42438fd587d
alwayiskarnis <133117494+wayis26@users.noreply.github.com>
2025-04-14 17:30:50 +0700

    perubahan project dan inisasi mobile interface

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,46 +7,52 @@ const Projects = () => {
                 <h1>Work/</h1>
             </div>
             <div className="column">
-                <div className="card">
-                    <div className="card-a">
-                        <div className="card-a-a">
-                            <h1>Redesplan APP</h1>
-                            <p>The main correct of corespondence</p>
+                <div className="ca">
+                    <div className="card">
+                        <div className="card-a">
+                            <div className="card-a-a">
+                                <h1>Verbique AI</h1>
+                                <p>The generative correction writings</p>
+                            </div>
+                            <div className="card-a-b">
+                                <span>April 13' - 2025 AI Generative Model</span>
+                            </div>
                         </div>
-                        <div className="card-a-b">
-                            <span>July 20' - 2025 The Caucasian Model</span>
+                        <div className="card-b">
+                            <div className="pro"></div>
                         </div>
-                    </div>
-                    <div className="card-b">
-                        <div className="pro"></div>
                     </div>
                 </div>
-                <div className="card">
-                    <div className="card-a">
-                        <div className="card-a-a">
-                            <h1>Redesplan APP</h1>
-                            <p>The main correct of corespondence</p>
+                <div className="ca">
+                    <div className="card">
+                        <div className="card-a">
+                            <div className="card-a-a">
+                                <h1>Redesplan APP</h1>
+                                <p>The main correct of corespondence</p>
+                            </div>
+                            <div className="card-a-b">
+                                <span>April 13' - 2025 AI Generative Model</span>
+                            </div>
                         </div>
-                        <div className="card-a-b">
-                            <span>July 20' - 2025 The Caucasian Model</span>
+                        <div className="card-b">
+                            <div className="pro"></div>
                         </div>
-                    </div>
-                    <div className="card-b">
-                        <div className="pro"></div>
                     </div>
                 </div>
-                <div className="card">
-                    <div className="card-a">
-                        <div className="card-a-a">
-                            <h1>Redesplan APP</h1>
-                            <p>The main correct of corespondence</p>
+                <div className="ca">
+                    <div className="card">
+                        <div className="card-a">
+                            <div className="card-a-a">
+                                <h1>Redesplan APP</h1>
+                                <p>The main correct of corespondence</p>
+                            </div>
+                            <div className="card-a-b">
+                                <span>July 20' - 2025 The Caucasian Model</span>
+                            </div>
                         </div>
-                        <div className="card-a-b">
-                            <span>July 20' - 2025 The Caucasian Model</span>
+                        <div className="card-b">
+                            <div className="pro"></div>
                         </div>
-                    </div>
-                    <div className="card-b">
-                        <div className="pro"></div>
                     </div>
                 </div>
             </div>
